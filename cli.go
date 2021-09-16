9015b5ee94cc7c96dd5dc076950963ff2d6fbdb7
muzzammil <muzzammil.shahid.311@gmail.com>
2021-09-16 10:46:37 +0500

    use flag for url and realm

--- cli.go
+++ cli.go
@@ -5,39 +5,39 @@ import (
 )
 var (
 	subscribeCommand = kingpin.Command("subscribe", "subscribe a topic.")
-	URLArgSub   = subscribeCommand.Arg("url", "url").Required().String()
-	realmArgSub = subscribeCommand.Arg("realm", "realmSub").Required().String()
-	topicArgSub = subscribeCommand.Arg("topic", "topic name").Required().String()
+	URLFlagSub   = subscribeCommand.Flag("url", "url").Required().Short('u').String()
+	realmFlagSub = subscribeCommand.Flag("realm", "realmSub").Required().Short('r').String()
+	topicArgSub  = subscribeCommand.Arg("topic", "topic name").Required().String()
 
-	publishCommand  = kingpin.Command("publish", "publishing a topic.")
-	urlArgPub   = publishCommand.Arg("url", "url").Required().String()
-	realmArgPub = publishCommand.Arg("realm", "realmSub").Required().String()
-	topicArgPub     = publishCommand.Arg("topic", "topic name").Required().String()
+	publishCommand = kingpin.Command("publish", "publishing a topic.")
+	urlFlagPub   = publishCommand.Flag("url", "url").Required().Short('u').String()
+	realmFlagPub = publishCommand.Flag("realm", "realmSub").Required().Short('r').String()
+	topicArgPub  = publishCommand.Arg("topic", "topic name").Required().String()
 	argumentsArgPub = publishCommand.Arg("args","give the arguments").Strings()
-	kwargsFlagPub   = publishCommand.Flag("kwargs", "give the keyword arguments").Short('k').StringMap()
+	kwargsFlagPub   = publishCommand.Flag("kwarg", "give the keyword arguments").Short('k').StringMap()
 
-	registerCommand  = kingpin.Command("register", "registering a procedure.")
-	urlArgReg   = registerCommand.Arg("url", "url").Required().String()
-	realmArgReg = registerCommand.Arg("realm", "realmSub").Required().String()
-	topicArgReg = registerCommand.Arg("procedure", "procedure name").Required().String()
+	registerCommand = kingpin.Command("register", "registering a procedure.")
+	urlFlagReg   = registerCommand.Flag("url", "url").Required().Short('u').String()
+	realmFlagReg = registerCommand.Flag("realm", "realmSub").Required().Short('r').String()
+	topicArgReg  = registerCommand.Arg("procedure", "procedure name").Required().String()
 
-	callCommand  = kingpin.Command("call", "calling a procedure.")
-	urlArgCal   = callCommand.Arg("url", "url").Required().String()
-	realmArgCal = callCommand.Arg("realm", "realmSub").Required().String()
-	topicArgCal     = callCommand.Arg("procedure", "procedure name").Required().String()
+	callCommand = kingpin.Command("call", "calling a procedure.")
+	urlFlagCal   = callCommand.Flag("url", "url").Required().Short('u').String()
+	realmFlagCal = callCommand.Flag("realm", "realmSub").Required().Short('r').String()
+	topicArgCal  = callCommand.Arg("procedure", "procedure name").Required().String()
 	argumentsArgCal = callCommand.Arg("args","give the arguments").Strings()
-	kwargsFlagCal   = callCommand.Flag("kwargs", "give the keyword arguments").Short('k').StringMap()
+	kwargsFlagCal   = callCommand.Flag("kwarg", "give the keyword arguments").Short('k').StringMap()
 )
 
 func main() {
 	switch kingpin.Parse() {
 		case "subscribe":
-			subscribe(*URLArgSub, *realmArgSub, *topicArgSub)
+			subscribe(*URLFlagSub, *realmFlagSub, *topicArgSub)
 		case "publish":
-			publish(*urlArgPub, *realmArgPub, *topicArgPub, *argumentsArgPub, *kwargsFlagPub)
+			publish(*urlFlagPub, *realmFlagPub, *topicArgPub, *argumentsArgPub, *kwargsFlagPub)
 		case "register":
-			register(*urlArgReg, *realmArgReg, *topicArgReg)
+			register(*urlFlagReg, *realmFlagReg, *topicArgReg)
 		case "call":
-			call(*urlArgCal, *realmArgCal, *topicArgCal, *argumentsArgCal, *kwargsFlagCal)
+			call(*urlFlagCal, *realmFlagCal, *topicArgCal, *argumentsArgCal, *kwargsFlagCal)
 	}
 }
