cd39c700364cdcd058fb314aadbf7d1c4ae8c2f2
Omer Akram <om26er@gmail.com>
2021-10-24 23:00:16 +0500

    Fix WAMPCRA salting

--- wamp/main.go
+++ wamp/main.go
@@ -25,14 +25,16 @@ package wamp
 import (
 	"bytes"
 	"context"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
+	"golang.org/x/crypto/ed25519"
+	"golang.org/x/crypto/pbkdf2"
 	"log"
 	"os"
 	"os/exec"
 	"os/signal"
-
-	"encoding/hex"
-	"golang.org/x/crypto/ed25519"
 
 	"github.com/gammazero/nexus/v3/client"
 	"github.com/gammazero/nexus/v3/transport/serialize"
@@ -118,8 +120,35 @@ func ConnectCRA(url string, realm string, serializer serialize.Serialization, au
 		HelloDetails: helloDict,
 		AuthHandlers: map[string]client.AuthFunc{
 			"wampcra": func (c *wamp.Challenge) (string, wamp.Dict) {
-				sig := crsign.RespondChallenge(secret, c, nil)
-				return sig, wamp.Dict{}
+				ch, _ := wamp.AsString(c.Extra["challenge"])
+				// If the client needed to lookup a user's key, this would require decoding
+				// the JSON-encoded challenge string and getting the authid.  For this
+				// example assume that client only operates as one user and knows the key
+				// to use.
+				saltStr, _ := wamp.AsString(c.Extra["salt"])
+				// If no salt given, use raw password as key.
+				if saltStr == "" {
+					return crsign.SignChallenge(ch, []byte(secret)), wamp.Dict{}
+				}
+
+				// If salting info give, then compute a derived key using PBKDF2.
+				salt := []byte(saltStr)
+				iters, _ := wamp.AsInt64(c.Extra["iterations"])
+				keylen, _ := wamp.AsInt64(c.Extra["keylen"])
+
+				if iters == 0 {
+					iters = 1000
+				}
+				if keylen == 0 {
+					keylen = 32
+				}
+
+				// Compute derived key.
+				dk := pbkdf2.Key([]byte(secret), salt, int(iters), int(keylen), sha256.New)
+				// Get base64 bytes. see https://github.com/gammazero/nexus/issues/252
+				derivedKey := []byte(base64.StdEncoding.EncodeToString(dk))
+
+				return crsign.SignChallenge(ch, derivedKey), wamp.Dict{}
 			},
 		},
 		Serialization: serializer,
